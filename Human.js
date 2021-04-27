8408d1b29485858b0a129436ef41db14cf8c8b7e
Nguyen Thanh Dat <topthuthuat69@gmail.com>
2021-04-27 16:39:16 +0700

    Khởi tạp class Apple Khởi tạo calss Human

---
+++ Human.js
@@ -0,0 +1,49 @@
+class Human {
+    constructor(name, gender, weight) {
+        this.name = name;
+        this.gender = gender;
+        this.weight = weight;
+
+    }
+
+    isMale() {
+        return this.gender == MALE;
+    }
+
+    setGender(gender) {
+        this.gender = gender;
+    }
+
+    checkApple(Apple) {
+        if (Apple.isEmpty()) {
+            alert("Toi da an het");
+        } else {
+            alert("Toi con " + Apple.getWeight());
+        }
+    }
+
+    eat(Apple) {
+        Apple.decrease();
+        this.weight++;
+    }
+
+    say(message) {
+        alert(message);
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    setName(name) {
+        this.name = name;
+    }
+
+    getWeight() {
+        return this.weight;
+    }
+
+    setWeight(weight) {
+        this.weight = weight;
+    }
+}
